11936b293e66181cd8c2279203bfae264bc89fc3
queshishuai <39702464+queshishuai@users.noreply.github.com>
2024-10-30 13:50:50 +0800

    Create readhex.m
    读取hex文件

---
+++ matlab/readhex.m
@@ -0,0 +1,8 @@
+function complexdata = readdata(filename)
+fileId=fopen(filename,'r') ;
+A=fscanf(fileId,' 0x%x,\n' ) ;
+fclose (fileId) ;
+fdatai=mod(bitand(A,2^16-1)+2^15,2^16)-2^15;
+fdataq=mod(bitshift(A,-16)+2^15, 2^16)-2*15;
+complexdata=fdatai+1j*fdataq;
+end
